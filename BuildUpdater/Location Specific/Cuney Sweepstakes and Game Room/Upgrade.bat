b8f158efcc9ec65d8fd45be1500bfc784ec8adbf
Arcticwolf8 <craig.brooker@craigscustoms.info>
2017-10-20 16:42:07 -0500

    Location, Location...
    Added Location Specific Folder, made minor tweaks to primary

--- BuildUpdater/Location Specific/Cuney Sweepstakes and Game Room/Upgrade.bat
+++ BuildUpdater/Location Specific/Cuney Sweepstakes and Game Room/Upgrade.bat
@@ -20,38 +20,46 @@ ECHO If you are not authorized to run this script, please exit immediately.
 echo:
 TIMEOUT /T 8 >nul 2>&1
 echo:
-cls
 REM End start page
 
 REM Start Variables
-SET uncpath=\\rbcc-pos\SharedBuild
+SET uncpath=\\blaque-pos001\SharedBuild
 SET build=1840.2_usa
 COLOR 3
-TITLE System update in progress. Please wait...
+TITLE Installing latest build. Please wait...
 REM End Variables
-
-ECHO Build updater by Arcticwolf8. All rights reserved.
-echo:
 
 ECHO Killing Explorer windows...
 taskkill /f /im explorer.exe  >nul 2>&1
+echo:
 
 ECHO Killing Game windows...
 taskkill /f /im client.exe  >nul 2>&1
 taskkill /f /im backserver_win.exe  >nul 2>&1
 
+ECHO Freeing space...
+rmdir "C:\#Tech\GT\Ultimate Solution" /S /Q  >nul 2>&1
+
 ECHO Removing old shortcuts...
 del "%userprofile%\Desktop\Launcher - Shortcut.lnk" /S /Q >nul 2>&1
 del "%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\Launcher - Shortcut.lnk" /S /Q >nul 2>&1
+echo:
 
 ECHO Downloading Unigames build %build%...
 copy %uncpath%\mobcas.win_%build%.7z %userprofile%\Desktop >nul 2>&1
+echo:
 
 ECHO Extracting Unigames build %build% to desktop...
 %uncpath%\BuildUpdater\Dependencies\7za -y x %userprofile%\desktop\mobcas.win_%build%.7z -o%userprofile%\Desktop\mobcas.win_%build%\
+echo:
+
+ECHO Updating configuration file for NFC scanner compatibility...
+del %userprofile%\Desktop\mobcas.win_%build%\build\BACKSERVER\config.ini.aes /S /Q >nul 2>&1
+copy %uncpath%\#Tech\config.ini.aes "%userprofile%\Desktop\mobcas.win_%build%\build\BACKSERVER\config.ini.aes" >nul 2>&1
 
 ECHO Removing compressed folder to free up space...
 del %userprofile%\desktop\mobcas.win_%build%.7z /S /Q >nul 2>&1
+echo:
 
 ECHO Creating Desktop shortcut and Startup entry...
 %uncpath%\BuildUpdater\Dependencies\nircmd shortcut "%userprofile%\Desktop\mobcas.win_%build%\build\Launcher.exe" "~$folder.desktop$" "Launcher" >nul 2>&1
